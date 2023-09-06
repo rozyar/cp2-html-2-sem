5782361b47117f46f045d7f1344592a0e28d97aa
rozyar <rayzor@arboleya.me>
2023-09-06 17:50:23 -0300

    Added useParams and useNavigatee to EditarProduto.jsx

--- src/routes/EditarProdutos/EditarProdutos.jsx
+++ src/routes/EditarProdutos/EditarProdutos.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import ListaProdutos from '../../components/ListaProdutos';
 
 const EditarProdutos = () => {
+    const lista = ListaProdutos;
+    const navegacao = useNavigate();
+    const {id} = useParams();
+
+    const proc = lista.filter(prod => prod.id == id)
+    const produto = proc[0];
+
     return (
         <main>
             <h1>Editar Produtos</h1>
+            <p>Editando os dados do produto: produto {produto.name}</p>
+            <button onClick={salvar}>Salvar</button>
         </main>
     );
 }
--- src/routes/Produtos/Produtos.jsx
+++ src/routes/Produtos/Produtos.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import ListaProdutos from '../../components/ListaProdutos';
+import { Link } from 'react-router-dom';
 
 const Produtos = () => {
     return (
         <main>
             <h1>Produtos</h1>
+            {ListaProdutos.map(prod => (
+                <div key = {prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar o produto : {prod.nome}
+                    </Link>
+                </div>
+            ))}
         </main>
     );
 }
