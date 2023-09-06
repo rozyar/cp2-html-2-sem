eed2f1a0bc208df01836a72e83eaccfcabffcc55
rozyar <rayzor@arboleya.me>
2023-09-06 17:03:07 -0300

    Route EditarProdutos Created

---
+++ src/routes/EditarProdutos/EditarProdutos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditarProdutos = () => {
+    return (
+        <div>
+            <h1>Editar Produtos</h1>
+        </div>
+    );
+}
+
+export default EditarProdutos;
+
