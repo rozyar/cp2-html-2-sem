c6d7eb244fd13af90537630cc1352d8f45a93482
rozyar <rayzor@arboleya.me>
2023-09-06 17:38:43 -0300

    ListraProdutos created and added objects

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ListaProdutos = [
+    {
+        id: 1,
+        nome: 'Teclado',
+        preco: 100,
+    },{
+        id: 2,
+        nome: 'Mouse',
+        preco: 50,
+    },{
+        id: 3,
+        nome: 'Monitor',
+        preco: 500,
+    },
+]
+
+export default ListaProdutos;
