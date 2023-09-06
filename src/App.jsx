a6a8d741710dc2e2963b8ea16b4a346340e2fb87
rozyar <rayzor@arboleya.me>
2023-09-06 17:30:35 -0300

    Menu And Rodape components added into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import Menu from './components/Menu'
+import Rodape from './components/Rodape'
 
 function App() {
   
 
   return (
     <>
+      <Menu />
       <Outlet />
+      <Rodape />
     </>
   )
 }
